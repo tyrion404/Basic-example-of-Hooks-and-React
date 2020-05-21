4fda0c50ee27a273cf68d2feb21780063acbe06b
tyrion404 <chintan.170410116089@gmail.com>
2020-05-21 18:02:07 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                Hooks and React.
+                <br />
+                Basic example of how hook works.
+            </div >
+        );
+    }
+}
+
+export default App;
